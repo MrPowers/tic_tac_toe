abdca298cb8989749366d42123f6da20273626f1
Anna Baik <annaheyonbaik@gmail.com>
2014-08-21 12:27:01 +0100

    Convert specs to RSpec 2.99.2 syntax with Transpec
    This conversion is done by Transpec 2.3.6 with the following command:
        transpec
    
    * 10 conversions
        from: obj.stub(:message)
          to: allow(obj).to receive(:message)
    
    * 4 conversions
        from: be_false
          to: be_falsey
    
    * 4 conversions
        from: be_true
          to: be_truthy
    
    * 2 conversions
        from: Klass.any_instance.stub(:message)
          to: allow_any_instance_of(Klass).to receive(:message)
    
    * 2 conversions
        from: expect(collection).to have(n).items
          to: expect(collection.size).to eq(n)
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -3,29 +3,29 @@ require "spec_helper"
 describe Array do
   context "#all_empty?" do
     it "returns true if all elements of the Array are empty" do
-      expect(["", "", ""].all_empty?).to be_true
+      expect(["", "", ""].all_empty?).to be_truthy
     end
 
     it "returns false if some of the Array elements are not empty" do
-      expect(["", 1, "", Object.new, :a].all_empty?).to be_false
+      expect(["", 1, "", Object.new, :a].all_empty?).to be_falsey
     end
 
     it "returns true for an empty Array" do
-      expect([].all_empty?).to be_true
+      expect([].all_empty?).to be_truthy
     end
   end
 
   context "#all_same?" do
     it "returns true if all elements of the Array are the same" do
-      expect(["A", "A", "A"].all_same?).to be_true
+      expect(["A", "A", "A"].all_same?).to be_truthy
     end
 
     it "returns false if some of the Array elements are not the same" do
-      expect(["", 1, "", Object.new, :a].all_same?).to be_false
+      expect(["", 1, "", Object.new, :a].all_same?).to be_falsey
     end
 
     it "returns true for an empty Array" do
-      expect([].all_same?).to be_true
+      expect([].all_same?).to be_truthy
     end
   end
 
