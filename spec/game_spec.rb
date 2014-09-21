99fb5fb5838d479285d6c55d1d3bda672cd54c0c
Matthew Powers <matthewkevinpowers@gmail.com>
2014-09-21 19:26:18 -0400

    Added one stub and one mock for examples

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -44,6 +44,13 @@ module TicTacToe
         expected = "bob: Enter a number between 1 and 9 to make your move"
         expect(game.solicit_move).to eq expected
       end
+
+      it "generates the right message" do
+        game = Game.new([bob, frank])
+        game.stub_chain(:current_player, :name).and_return "frank"
+        expected = "frank: Enter a number between 1 and 9 to make your move"
+        expect(game.solicit_move).to eq expected
+      end
     end
 
     context "#get_move" do
@@ -55,6 +62,12 @@ module TicTacToe
       it "converts human_move of '1' to [0, 0]" do
         game = Game.new([bob, frank])
         expect(game.get_move("7")).to eq [0, 2]
+      end
+
+      it "calls #human_move_to_coordinate" do
+        game = Game.new([bob, frank])
+        game.should_receive(:human_move_to_coordinate).with("x")
+        game.get_move("x")
       end
     end
 
